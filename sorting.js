25b9b2a2b85a9c63750b85f7415bf42ca11bc216
Lenkevich Volha <lenkevich@hotmail.com>
2018-05-08 21:18:29 +0300

    sorting.js is added

---
+++ sorting.js
@@ -0,0 +1,19 @@
+var birds = [{
+	name: 'parrot',
+	age: 7
+}, {
+	name: 'swan',
+	age: 5
+}, {
+	name: 'parakeet',
+	age: 1
+}, {
+	name: 'owl',
+	age: 2
+}
+];
+
+birds.sort(function(a, b) {
+	return a.age - b.age 
+});
+console.log(birds);
